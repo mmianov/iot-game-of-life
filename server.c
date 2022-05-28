4947d5684157025367ff319bf7ced353e6ab6dd1
mmianov <mateuszmianovany@gmail.com>
2022-05-28 20:48:03 +0200

    server from scratch, function to init and recv

--- server.c
+++ server.c
@@ -12,69 +12,58 @@
 #include <time.h>
 #include <sys/time.h>
 
-#define MAX_BUFF 128
 #define SERVER_PORT 17210
+#define MAX_MSG_SIZE 50
+
+int server_socket;
 
 
+// initializes UDP server
+int initialize_server(int port, int *server_socket){
+
+    // get file descriptor for server socket
+    *server_socket = socket(AF_INET, SOCK_DGRAM,0);
+    // check for errors
+    if (*server_socket < 0){
+        perror("Socket: ");
+        return -1
+    }
+
+    // create data structure to hold server parameters
+    struct sockaddr_in server_addr;
+    memset(&server_addr,0,sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(SERVER_PORT);
+
+    // assign the address specified by server_addr to server_socket file descriptor
+    if(bind(*listen_socket,(struct sockaddr*)&)server_addr, sizeof(struct sockaddr) != 0){
+        perror("Bind:");
+        return -1;
+    }
+
+    printf("Server initialized on %s:%d\n",inet_ntoa(server_addr.sin_addr), ntohs(server_addr.sin_port))
+}
+
+// receives data on server socket
+void receive_data(int *server_socket, char *message){
+    struct sockaddr_in node_addr;
+
+    // receive incoming data
+    int received_data = recvfrom(*server_socket, *message, MAX_MSG_SIZE, 0, (struct sockaddr*)&node_addr, &sizeof(node_addr));
+}
 
 int main(){
+    if (initialize_server(&server_socket) != 0){
+        exit(EXIT_FAILURE);
+    }
 
-
-	// struct sockaddr_in = trzyma typ IP, adres IP i port połączenia
-	struct sockaddr_in serveraddr, clientaddr;
-	int server_socket,  clientaddr_len=sizeof(clientaddr);
-	int activity;
-
-	unsigned char received_msg[MAX_BUFF];
-	unsigned char msg[MAX_BUFF];
-
-
-	fd_set read_fds;
-	memset(&serveraddr, 0, sizeof(serveraddr));
-	memset(&clientaddr, 0, sizeof(clientaddr));
-
-    // tworzenie deskryptora serwera UDP
-	if((server_socket = socket(AF_INET, SOCK_DGRAM, 0)) == -1){
-		printf("ERROR %s (%s:%d) \n", strerror(errno), __FILE__,__LINE__);
-	}
-
-	serveraddr.sin_family = AF_INET;
-	serveraddr.sin_addr.s_addr = INADDR_ANY;
-	serveraddr.sin_port = htons(SERVER_PORT);
-
-    // przypisanie deskryptora serwera do
-	if(bind(server_socket, (const struct sockaddr *)&serveraddr, sizeof(serveraddr)) != 0){
-		printf("ERROR %s (%s:%d) \n", strerror(errno), __FILE__,__LINE__);
-	}
-
-	struct timeval timeout;
-	timeout.tv_sec = 0;
-	timeout.tv_usec = 1000;
-
-
-	for(;;) {
-
-		FD_ZERO(&read_fds);
-		FD_SET(server_socket, &read_fds);
-		activity = select(server_socket+1, &read_fds, NULL, NULL, &timeout);
-
-        // jeśli nowe połączenie (?)
-		if(FD_ISSET(server_socket, &read_fds)) {
-			int received_data = recvfrom(server_socket, msg, MAX_BUFF, 0, (struct sockaddr*)&clientaddr, &clientaddr_len);
-			if(received_data<0) {
-				printf("ERROR %s (%s:%d) \n", strerror(errno));
-				exit(-4);
-			}
-			printf("New node connection: %s\n",msg);
-		}
-		else{
-		    int received_data = recvfrom(server_socket, msg, MAX_BUFF, 0, (struct sockaddr*)&clientaddr, &clientaddr_len);
-				msg[received_data]='\0';
-				printf("From: %s:%d: %s\n", inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port), msg);
-		}
+    while(1){
+        char received_message[MAX_MSG_SIZE];
+        receive_data(&server_socket, received_message);
+        printf("Received message: %s", received_message);
 
     }
 
-	close(server_socket);
 
 }
