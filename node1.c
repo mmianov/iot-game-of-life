bb71ba34bb358440b64f64a01ea2a21f6a7b5030
mmianov <mateuszmianovany@gmail.com>
2022-05-28 17:49:54 +0200

    test server and node

---
+++ node1.c
@@ -0,0 +1,55 @@
+gi#include <ZsutEthernet.h>
+#include <ZsutEthernetUdp.h>
+#include <ZsutFeatures.h>
+#include <time.h>
+#include <string.h>
+#include <stdio.h>
+
+#define UDP_REMOTE_PORT 17210
+#define LOCAL_PORT 4444
+
+#define MAX_BUFFER 64
+#define REQUEST_BUFFER 5
+
+byte mac[] =  {0x01,0x23,0x09,0x67,0x89,0xab};
+ZsutEthernetUDP Udp;
+ZsutIPAddress serverIP = ZsutIPAddress(10,0,3,158);
+
+unsigned int localPort = LOCAL_PORT;
+unsigned int remotePort = UDP_REMOTE_PORT;
+
+unsigned char requestBuffer[REQUEST_BUFFER];
+unsigned char packetBuffer[MAX_BUFFER];
+
+int helloReceived = 0;
+uint16_t measure;
+uint32_t timer;
+
+
+
+void millis_delay(unsigned long delay){
+	unsigned long timer = ZsutMillis() + delay;
+	for(;;){
+	   if(timer < ZsutMillis()) break;
+	}
+}
+
+
+void setup(){
+    Serial.begin(9600);
+    ZsutEthernet.begin(mac);
+    Serial.println(ZsutEthernet.localIP());
+    Udp.begin(localPort);
+    Serial.println("Node up and running ... ");
+
+    requestBuffer[5] = "test";
+}
+
+void loop(){
+
+  	Udp.beginPacket(serverIP, remotePort);
+	int w = Udp.write(requestBuffer, REQUEST_BUFFER);
+	Udp.endPacket();
+
+
+}
---
+++ server.c
@@ -0,0 +1,77 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <errno.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/select.h>
+#include <time.h>
+#include <sys/time.h>
+
+#define MAX_BUFF 128
+#define SERVER_PORT 17210
+
+
+
+int main(){
+
+
+	// struct sockaddr_in = trzyma typ IP, adres IP i port połączenia
+	struct sockaddr_in serveraddr, clientaddr;
+	int server_socket,  clientaddr_len=sizeof(clientaddr);
+	int activity;
+
+	unsigned char received_msg[MAX_BUFF];
+	unsigned char msg[MAX_BUFF];
+
+
+	fd_set read_fds;
+	memset(&serveraddr, 0, sizeof(serveraddr));
+	memset(&clientaddr, 0, sizeof(clientaddr));
+
+    // tworzenie deskryptora serwera UDP
+	if((server_socket = socket(AF_INET, SOCK_DGRAM, 0)) == -1){
+		printf("ERROR %s (%s:%d) \n", strerror(errno), __FILE__,__LINE__);
+	}
+
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_addr.s_addr = INADDR_ANY;
+	serveraddr.sin_port = htons(SERVER_PORT);
+
+    // przypisanie deskryptora serwera do
+	if(bind(server_socket, (const struct sockaddr *)&serveraddr, sizeof(serveraddr)) != 0){
+		printf("ERROR %s (%s:%d) \n", strerror(errno), __FILE__,__LINE__);
+	}
+
+
+	for(;;) {
+
+		FD_ZERO(&read_fds);
+		FD_SET(server_socket, &read_fds);
+		activity = select(server_socket+1, &read_fds, NULL, NULL, 100);
+
+        // jeśli nowe połączenie (?)
+		if(FD_ISSET(server_socket, &read_fds)) {
+			int received_data = recvfrom(server_socket, msg, MAX_BUFF, 0, (struct sockaddr*)&clientaddr, &clientaddr_len);
+			if(received_data<0) {
+				printf("ERROR %s (%s:%d) \n", strerror(errno));
+				exit(-4);
+			}
+			printf("New node connection: %s\n",msg);
+		}
+		else{
+		    int received_data = recvfrom(server_socket, msg, MAX_BUFF, 0, (struct sockaddr*)&clientaddr, &clientaddr_len);
+				msg[received_data]='\0';
+				printf("From: %s:%d: %s\n", inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port), msg);
+		}
+
+    }
+
+	freeaddrinfo(res);
+	close(server_socket);
+
+}
