8e096c133dea69143c79a5d588e2cee32941c393
mmianov <mateuszmianovany@gmail.com>
2022-06-06 00:43:08 +0200

    protocol message try 1

--- server.c
+++ server.c
@@ -409,9 +409,13 @@ int main(){
     memset(&protocol_message,0,sizeof(protocol_message));
     //protocol_message[0] = BOUNDARY_UPDATE_CODE;
 
-    int *protocol_message_test = (int*) game_nodes[0].area;
+    //int *protocol_message_test = (int*) game_nodes[0].area;
+    char protocol_message[node_area_rows*node_area_cols];
+    for(int i=0;i<node_area_rows*node_area_cols;i++){
+        protocol_message[i] = *(*game_nodes[0].area + i);
+    }
 
-    sendto(server_socket, protocol_message_test, sizeof(protocol_message_test), 0, (struct sockaddr *)&game_nodes[0].net_addr, addr_len);
+    sendto(server_socket, protocol_message, sizeof(protocol_message), 0, (struct sockaddr *)&game_nodes[0].net_addr, addr_len);
 
 
 
