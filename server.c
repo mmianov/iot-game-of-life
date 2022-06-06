f2c66077c191476d4b6ad028e09039df8c0825e5
mmianov <mateuszmianovany@gmail.com>
2022-06-06 02:38:28 +0200

    write to buffer function 1

--- server.c
+++ server.c
@@ -424,6 +424,9 @@ int main(){
     //int *protocol_message_test = (int*) game_nodes[0].area;
     char protocol_message[node_area_rows*node_area_cols];
     write_to_buffer(protocol_message,(int*)area1,node_area_rows,node_area_cols);
+    for(int i=0;i<node_area_rows*node_area_cols;i++){
+        printf("%c",protocol_message[i]);
+    }
 
 //    for(int i=0;i<node_area_rows*node_area_cols;i++){
 //
